c54936ae4f2b6242a9e3c1ef8dedc84aa0f67f1a
odify <odify495@gmail.com>
2020-06-08 20:56:33 +0200

    xyz

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,3 @@
-
 // RESET CALCULATOR
 
 function form_reset() {
@@ -24,4 +23,59 @@ function update(value) {
 // OPERATORS DO NOT REPEAT
 
 // function notRepeat {}
+
+// function button(x) {
+
+
+
+
+
+
+
+
+//     let display = document.getElementById("display"),
+//         dispplayLength = display.value.length
+
+
+
+
+//     if (display.value == 0) {
+//         display.value = ""
+//     }
+// if (x !== "c" && x !== "+/-" && x !== "=" && x !== "." && dispplayLength <= 20) {
+//             display.value += x
+//     }
+
+
+//     // C button
+//     if (x == "c") {
+//         display.value = "0"
+//     }
+
+//     // = button
+//     if (x == "=") {
+//         display.value = eval(display.value)
+//     }
+
+//     // . button
+//     if (x == "." && display.value.split(".").length < 2 && display.value !== 0) {
+//         display.value += "."
+//     } else if (x == "." && display.value == 0) {
+//         display.value = 0
+//     }
+
+//     // DEL button
+//     let oneLess = display.value.length - 4
+//     if (x == "del" && display.value !== 0 && display.value.length > 1) {
+//             display.value = display.value.substring(0, oneLess)
+//     }
+
+
+
+//     // +/- button
+//     if (x == "+/-" && display.value !== 0) {
+//             let negative = (display.value - display.value) - display.value
+//             display.value = negative
+//     }
+// }
 
